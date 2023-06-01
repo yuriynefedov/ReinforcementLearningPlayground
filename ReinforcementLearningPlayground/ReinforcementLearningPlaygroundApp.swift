54f25ef35c323f5c5072708a09be9b60d54859a5
Yuriy Nefedov <yurii.nefedov@ucu.edu.ua>
2023-06-01 15:50:12 +0300

    Initial Commit

---
+++ ReinforcementLearningPlayground/ReinforcementLearningPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReinforcementLearningPlaygroundApp.swift
+//  ReinforcementLearningPlayground
+//
+//  Created by Yuriy Nefedov on 01.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct ReinforcementLearningPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
